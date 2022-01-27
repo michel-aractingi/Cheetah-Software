a407062bd1bd3526ba7b772e6d3727c4dbb2e38c
Tomi Silander <tsilande@celebes.ad.europe.naverlabs.com>
2022-01-27 10:55:10 +0100

    Latest controller brige now.

--- user/ControllerBridge/ControllerBridge.cpp
+++ user/ControllerBridge/ControllerBridge.cpp
@@ -7,6 +7,7 @@ void ControllerBridge::runController() {
   for(u32 leg = 0; leg < 4; leg++) {
     for(u32 joint = 0; joint < 3; joint++) {
       _legController->commands[leg].qDes[joint] = command.q_des[idx];
+      _legController->commands[leg].qdDes[joint] = command.qd_des[idx];
       _legController->commands[leg].kpJoint(joint,joint) = command.kp_joint[idx];
       _legController->commands[leg].kdJoint(joint,joint) = command.kd_joint[idx];
       _legController->commands[leg].tauFeedForward[joint] = command.tau_ff[idx];
